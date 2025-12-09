d446dcdfba0d6366332980b1dd405b9c97d32228
rag5798 <bgoettman@gmail.com>
2025-12-09 18:55:34 -0500

    working OAUTH and routes

--- routes/list.js
+++ routes/list.js
@@ -1,123 +1,47 @@
 const router = require("express").Router();
-const listController = require("../controllers/listController");
 const requireAuth = require("../middleware/requireauth");
+const listsController = require("../controllers/listController");
 
-router.use(requireAuth);
-
-//------------- Item CRUD Routes ------------------//
+//router.use(requireAuth);
 
 // Create
 router.post(
-  "/list",
+  "/lists",
   /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Create a new inbox list item' */
-  /* #swagger.description = 'Create a new to-do item in the main inbox list.' */
-  listController.createItem
+  /* #swagger.summary = 'Create a new list' */
+  listsController.createList
 );
 
 // Read all
 router.get(
-  "/list",
+  "/lists",
   /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Get all inbox list items' */
-  /* #swagger.description = 'Return every to-do item in the main inbox list.' */
-  listController.getAllItems
+  /* #swagger.summary = 'Get all lists for the current user' */
+  listsController.getLists
 );
 
-// Read by id
+// Read one
 router.get(
-  "/list/:id",
+  "/lists/:id",
   /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Get a single list item' */
-  /* #swagger.description = 'Look up a to-do item by its MongoDB ObjectId.' */
-  listController.getItemById
+  /* #swagger.summary = 'Get a single list by id' */
+  listsController.getListById
 );
 
 // Update
 router.patch(
-  "/list/:id",
+  "/lists/:id",
   /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Update a list item' */
-  /* #swagger.description = 'Partially update the title and/or description of a list item.' */
-  listController.updateItem
+  /* #swagger.summary = 'Update a list' */
+  listsController.updateList
 );
 
 // Delete
 router.delete(
-  "/list/:id",
+  "/lists/:id",
   /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Delete a list item' */
-  /* #swagger.description = 'Permanently delete a list item from the inbox list.' */
-  listController.deleteItem
-);
-
-//------------- Item Status Routes ------------------//
-
-// Doing
-router.get(
-  "/list/doing",
-  /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Get all Doing items' */
-  /* #swagger.description = 'Return all items currently in the Doing list.' */
-  listController.getDoingItems
-);
-
-router.put(
-  "/list/doing/:id",
-  /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Update a Doing item' */
-  /* #swagger.description = 'Update the title and/or description of a Doing item.' */
-  listController.updateDoingItem
-);
-
-router.delete(
-  "/list/doing/:id",
-  /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Delete a Doing item' */
-  /* #swagger.description = 'Remove an item from the Doing list.' */
-  listController.deleteDoingItem
-);
-
-// Done
-router.get(
-  "/list/done",
-  /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Get all Done items' */
-  /* #swagger.description = 'Return all items that have been marked Done.' */
-  listController.getDoneItems
-);
-
-router.delete(
-  "/list/done/:id",
-  /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Delete a Done item' */
-  /* #swagger.description = 'Remove an item from the Done list.' */
-  listController.deleteDoneItem
-);
-
-// Delegate
-router.get(
-  "/list/delegate",
-  /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Get all Delegate items' */
-  /* #swagger.description = 'Return all items that have been delegated.' */
-  listController.getDelegateItems
-);
-
-router.put(
-  "/list/delegate/:id",
-  /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Update a Delegate item' */
-  /* #swagger.description = 'Update the title and/or description of a delegated item.' */
-  listController.updateDelegateItem
-);
-
-router.delete(
-  "/list/delegate/:id",
-  /* #swagger.tags = ['List'] */
-  /* #swagger.summary = 'Delete a Delegate item' */
-  /* #swagger.description = 'Remove an item from the Delegate list.' */
-  listController.deleteDelegateItem
+  /* #swagger.summary = 'Delete a list' */
+  listsController.deleteList
 );
 
 module.exports = router;
